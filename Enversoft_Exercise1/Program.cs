40f176bcc4eea324e1dab6286decc9ce9f94c617
mlando2.0 <mlando2.0@outlook.com>
2023-07-09 05:05:30 +0200

    Add regions for each question

--- Enversoft_Exercise1/Program.cs
+++ Enversoft_Exercise1/Program.cs
@@ -9,15 +9,33 @@ namespace ConsoleApplication1
             int[] list1 = new int[] { 1, 2, 3, 4, 5 };
             int[] list2 = new int[] { 3, 4, 5, 6, 7 };
             //a. Show the common elements in both lists. E.g the common elements are "3 4 5", because they are contained in both lists.
+            
+            #region A - Common Elements
+
             int[] list3 = new int[] { };
             Console.WriteLine(string.Join(" ", list3));
+
+            #endregion A - Common Elements
+
             //b. Show the elements from list 1, but is not found in list2. E.g 1 2"
+
+            #region B - Exclude all but list 1
+
             int[] list4 = new int[] { };
             Console.WriteLine(string.Join(" ", list4));
+
+            #endregion B - Exclude all but list 1
+
             //c. Complete here
             //Show the elements from list 2, but is not found in list1. E.g 6 7"
+
+            #region C - Exclude all but list 2
+
             int[] list5 = new int[] { };
             Console.WriteLine(string.Join(" ", list5));
+
+            #endregion C - Exclude all but list 2
+
             Console.WriteLine("Press <ENTER> to continue");
             Console.ReadLine();
         }
